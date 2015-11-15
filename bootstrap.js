8251ca410266a8931e27ffcfd68c7743227adefd
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2015-11-15 23:22:27 +0100

    Factory registration
    Successful factory registration however error with createInstance. May
    be because it is not possible to implement the calICalendar with
    restartless extension.

--- bootstrap.js
+++ bootstrap.js
@@ -3,6 +3,7 @@ Components.utils.import("resource://gre/modules/Services.jsm");
 Components.utils.import("resource://gre/modules/FileUtils.jsm");
 Components.utils.import("resource://todotxt/modules/logger.jsm");
 Components.utils.import("resource://todotxt/components/calTodotxt.js");
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
 function install(aData,aReason){
 
@@ -11,34 +12,41 @@ function install(aData,aReason){
 function uninstall(){}
 
 function startup(aData,aReason){
-  todotxtLogger.debugMode = true;
-  todotxtLogger.debug("calTodoTxt");
-	//let resource = Services.io.getProtocolHandler("resource")
-	 // .QueryInterface(Components.interfaces.nsIResProtocolHandler);
-  //jlet alias = Services.io.newFileURI(aData.installPath);
-  //if (!aData.installPath.isDirectory())
-   // alias = Services.io.newURI("jar:" + alias.spec + "!/", null, null);
+
+	let resource = Services.io.getProtocolHandler("resource")
+         .QueryInterface(Components.interfaces.nsIResProtocolHandler);
+
+  let alias = Services.io.newFileURI(aData.installPath);
+  if (!aData.installPath.isDirectory())
+    alias = Services.io.newURI("jar:" + alias.spec + "!/", null, null);
   resource.setSubstitution("todotxt", alias);
 
-  var factory = Factory({
-      contract: '@mozilla.org/calendar/calendar;1?type=todotxt',
-      Component: calTodoTxt
-  });
+  var compman=Components.manager;
+  let comreg = compman.QueryInterface(Components.interfaces.nsIComponentRegistrar);
 
+  todotxtLogger.debugMode = true;
+  todotxtLogger.debug("calTodoTxt");
 
-  tmp_str = aData.installPath.path+"/components/calTodotxt.manifest";
-  todotxtLogger.debug("path: "+tmp_str);
-  //let cal_manifest_path = Services.io.newURI(tmp_str,null,null);
-  //todotxtLogger.debug("path: "+cal_manifest_path);
-  let cal_manifest = new FileUtils.File(tmp_str);
-  //var compman=Components.manager;
-  //compman.QueryInterface(Components.interfaces.nsIComponentRegistrar);
-  //compman.autoRegister(cal_manifest);
-  //Components.manager.addBootstrappedManifestLocation(cal_manifest);
+  calTodo =  calTodoTxt.prototype;
+  var factory = XPCOMUtils.generateNSGetFactory([calTodoTxt]);
 
+  if(!comreg.isContractIDRegistered('@mozilla.org/calendar/calendar;1?type=todotxt'))
+    comreg.registerFactory(calTodo.classID, 'calTodoTxt', calTodo.contractID, factory);
+
+  if(!comreg.isContractIDRegistered('@mozilla.org/calendar/calendar;1?type=todotxt'))
+	  throw Components.Exception("Todo.txt calendar not registrated", Components.results.NS_ERROR_UNEXPECTED);
+
+	let found = false;
 	var calManager = cal.getCalendarManager();
 	var id = "{00C350E2-3F65-11E5-8E8B-FBF81D5D46B0}";
-	let found = false;
+	
+	if (!Components.classes[calTodo.contractID])
+		  throw Components.Exception("Todo.txt classes not found", Components.results.NS_ERROR_UNEXPECTED);
+
+	let tmp = Components.classes[calTodo.contractID].createInstance(Components.interfaces.calICalendar);
+  if(!tmp)
+		  throw Components.Exception("Todo.txt create instance failed", Components.results.NS_ERROR_UNEXPECTED);
+    
 
 	for each (calendar in calManager.getCalendars({})){
 		if(calendar.providerID == id){
@@ -50,6 +58,7 @@ function startup(aData,aReason){
 	if(!found){
 		let url = cal.makeURL('todotxt://_unused');
 		let newCal = calManager.createCalendar('todotxt',url);
+    todotxtLogger.debug("Calendar: "+newCal);
 		if(!newCal)
 		  throw Components.Exception("Could not create Todo.txt Calendar", Components.results.NS_ERROR_UNEXPECTED);
     
