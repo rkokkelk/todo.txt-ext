adb99c1d4b694a837ad2f5b5861fc2130e0746f6
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2016-05-08 19:47:22 +0200

    Initial cleanup of old norestart code

--- bootstrap.js
+++ bootstrap.js
@@ -13,28 +13,6 @@ function uninstall(){}
 function startup(aData,aReason){
   todotxtLogger.debugMode = true;
   todotxtLogger.debug("calTodoTxt");
-	//let resource = Services.io.getProtocolHandler("resource")
-	 // .QueryInterface(Components.interfaces.nsIResProtocolHandler);
-  //jlet alias = Services.io.newFileURI(aData.installPath);
-  //if (!aData.installPath.isDirectory())
-   // alias = Services.io.newURI("jar:" + alias.spec + "!/", null, null);
-  resource.setSubstitution("todotxt", alias);
-
-  var factory = Factory({
-      contract: '@mozilla.org/calendar/calendar;1?type=todotxt',
-      Component: calTodoTxt
-  });
-
-
-  tmp_str = aData.installPath.path+"/components/calTodotxt.manifest";
-  todotxtLogger.debug("path: "+tmp_str);
-  //let cal_manifest_path = Services.io.newURI(tmp_str,null,null);
-  //todotxtLogger.debug("path: "+cal_manifest_path);
-  let cal_manifest = new FileUtils.File(tmp_str);
-  //var compman=Components.manager;
-  //compman.QueryInterface(Components.interfaces.nsIComponentRegistrar);
-  //compman.autoRegister(cal_manifest);
-  //Components.manager.addBootstrappedManifestLocation(cal_manifest);
 
 	var calManager = cal.getCalendarManager();
 	var id = "{00C350E2-3F65-11E5-8E8B-FBF81D5D46B0}";
