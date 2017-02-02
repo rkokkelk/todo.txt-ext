c8eebed48da214e441595d8a84e1751ecac254df
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2017-02-02 13:50:38 +0100

    Added GPL2 license information to all files

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 #
-# Build script for add-on resulting in zip formmated file
-# todotxt_$version$.xpi
+# Build script for todo.txt-adon for Thunderbird email application
+# Output: todotxt_$version$.xpi
+# Copyright (C) 2017 Roy Kokkelkoren
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 function show_help {
   echo "usage: build.sh [-d | -h]"
