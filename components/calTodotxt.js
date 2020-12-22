1ea1136382eb721275f63273306998d01cc98a4b
RoyK <rkokk@protonmail.com>
2020-12-22 22:45:42 +0100

    Rewrite of TodoTxt calendar provider

--- components/calTodotxt.js
+++ components/calTodotxt.js
@@ -9,164 +9,105 @@ const { exception } = ChromeUtils.import('resource://todotxt/modules/exception.j
 const { todoClient } = ChromeUtils.import("resource://todotxt/modules/todoclient.jsm");
 const { todotxtLogger } = ChromeUtils.import("resource://todotxt/modules/logger.jsm");
 
-
-function calTodoTxt() {
-  this.initProviderBase();
-
-  todotxtLogger.debug("calTodoTxt", "Constructor");
-
-  observer_scope.prefObserver.register(this);
-  this.fileObserver = observer_scope.observers.registerFileObserver(this);
-}
-
-var calTodoCalendarclassID = Components.ID("{00C350E2-3F65-11E5-8E8B-FBF81D5D46B0}");
-var calTodoCalendarInterfaces = [Components.interfaces.calICalendar,
-                                 Components.interfaces.nsIClassInfo,
-                                 Components.interfaces.nsISupports];
-
 var EXPORTED_SYMBOLS = ["calTodoTxt"]; /* exported calTodoTxt */
 
-calTodoTxt.prototype = {
-   __proto__: cal.provider.BaseClass.prototype,
-  
-  classID: calTodoCalendarclassID,
-  QueryInterface: ChromeUtils.generateQI(calTodoCalendarInterfaces),
-  
-  flags: 0,
-  
-  mUri: null,
-  mLastSync: null,
-  mTaskCache: {},
-  mPendingApiRequest: false,
-  mPendingApiRequestListeners: {},
+class calTodoTxt extends cal.provider.BaseClass {
 
-  fileObserver: null,
+  static factory = null;
+  
+  QueryInterface = ChromeUtils.generateQI([
+    'calICalendar',
+    'nsIClassInfo',
+    'nsISupports'
+  ]);
+  
+  flags = 0;
+  
+  mUri = 'todotxt://_unused';
+  fileObserver = null;
 
-  get pendingApiRequest() {
-    return this.mPendingApiRequest;
-  },
+  constructor() {
+    super();
+    this.initProviderBase();
 
-  set pendingApiRequest(aValue) {
-    this.mPendingApiRequest = aValue;
-    
-    // when we turn off the pendingApiRequest flag, process anything in the pendingApiRequestListeners queue
-    if (aValue == false) {
-      let apiListeners = this.mPendingApiRequestListeners[this.id];
-      
-      for (let i=0; i<apiListeners.length; i++) {
-        let apiListener = apiListeners[i];
-        let callback = apiListener.callback;
-        callback.apply(this, [apiListener.itemFilter, apiListener.count, 
-                               apiListener.fromDate, apiListener.toDate, apiListener.listener]);
-      }
-      this.mPendingApiRequestListeners[this.id] = [];
-    }
-  },
+    observer_scope.prefObserver.register(this);
+    this.fileObserver = observer_scope.observers.registerFileObserver(this);
+  }
   
   get listId() {
     return this.getProperty('listId');
-  },
+  }
+
   set listId(aListId) {
     this.setProperty('listId', aListId);
-  },
+  }
   
   get itemType() {
     return this.getProperty('itemType');
-  },
+  }
 
-  setProperty: function(aName, aValue) {
+  setProperty(aName, aValue) {
       return this.__proto__.__proto__.setProperty.apply(this, arguments);
-  },
-  
-  getCachedItems: function cSC_getCachedItems(aItemFilter, aCount, aRangeStart, aRangeEnd, aListener) {
-    todotxtLogger.debug('calTodotxt.js:getCachedItems()');
-    
-    let items = [];
-    let taskCache = this.mTaskCache[this.id];
-
-    for (let i=0; i < taskCache.length; i++){
-      let itemId = taskCache[i];
-      let cachedItem = this.mTaskCache[this.id][itemId];
-      items.push(cachedItem);
-    }
-
-    aListener.onGetResult(this.superCalendar,
-                          Components.results.NS_OK,
-                          Components.interfaces.calITodo,
-                          null,
-                          items.length,
-                          items);
-    this.notifyOperationComplete(aListener, 
-                                  Components.results.NS_OK,
-                                  Components.interfaces.calIOperationListener.GET,
-                                  null,
-                                  null);
-  },
+  }
   
   /*
    * nsISupports
    */
   //TODO: find way for using global parametr
 
-  /*
-   * calICalendarProvider interface
-   */
   get prefChromeOverlay() {
     return null;
-  },
+  }
   
   get displayName() {
     return 'Todo.txt';
-  },
+  }
 
-  createCalendar: function cSC_createCal() {
+  createCalendar() {
     throw NS_ERROR_NOT_IMPLEMENTED;
-  },
+  }
 
-  deleteCalendar: function cSC_deleteCal(cal, listener) {
+  deleteCalendar(cal, listener) {
     throw NS_ERROR_NOT_IMPLEMENTED;
-  },
+  }
   
-  /*
-   * calICalendar interface
-   */
   get type() {
     return "todotxt";
-  },
+  }
 
   get providerID() {
     return "{00C350E2-3F65-11E5-8E8B-FBF81D5D46B0}";
-  },
+  }
 
   get canRefresh() {
     return true;
-  },
+  }
 
   get uri() {
     return this.mUri
-  },
+  }
+
   set uri(aUri) {
     this.mUri = aUri;
-  },
+  }
 
-  getProperty: function cSC_getProperty(aName) {
+  getProperty(aName) {
     return this.__proto__.__proto__.getProperty.apply(this, arguments);
-  },
+  }
     
-  refresh: function cSC_refresh() {
+  refresh() {
     todotxtLogger.debug('calTodotxt.js:refresh()');
     
     // setting the last sync to null forces the next getItems call to make an API request rather than returning a cached result
-    this.mLastSync = null;
     this.observers.notify("onLoad", [this]);
-  },
+  }
   
-  addItem: function cSC_addItem(aItem, aListener) {
+  addItem(aItem, aListener) {
     todotxtLogger.debug('calTodotxt.js:addItem()');
     return this.adoptItem(aItem.clone(), aListener);
-  },
+  }
   
-  adoptItem: function cSC_adoptItem(aItem, aListener) {
+  adoptItem(aItem, aListener) {
     todotxtLogger.debug('calTodotxt.js:adoptItem()');
     
     try {    
@@ -180,7 +121,6 @@ calTodoTxt.prototype = {
                                     Components.interfaces.calIOperationListener.ADD,
                                     item.id,
                                     item);
-      this.mTaskCache[this.id][item.id] = item;
       this.observers.notify("onAddItem", [item]);
       
       observer_scope.observers.fileEvent.updateMD5();
@@ -193,13 +133,12 @@ calTodoTxt.prototype = {
                                     null,
                                     e.message);
     }
-  },
+  }
   
-  modifyItem: function cSC_modifyItem(aNewItem, aOldItem, aListener) {
+  modifyItem(aNewItem, aOldItem, aListener) {
     todotxtLogger.debug('calTodotxt.js:modifyItem()');
     
     try{
-      this.mTaskCache[this.id][aNewItem.id] = aNewItem;
       todoClient.modifyItem(aOldItem, aNewItem);
     
       this.notifyOperationComplete(aListener,
@@ -207,7 +146,6 @@ calTodoTxt.prototype = {
                                    Components.interfaces.calIOperationListener.MODIFY,
                                    aNewItem.id,
                                    aNewItem);
-      this.mTaskCache[this.id][aNewItem.id] = aNewItem;
       this.observers.notify('onModifyItem', [aNewItem, aOldItem]);
       
       // Update checksum because file changes and thus
@@ -222,14 +160,13 @@ calTodoTxt.prototype = {
                                    null,
                                    e.message);
     }
-  },
+  }
   
-  deleteItem: function cSC_deleteItem(aItem, aListener) {
+  deleteItem(aItem, aListener) {
     todotxtLogger.debug('calTodotxt.js:deleteItem()');
     
     try {
       todoClient.deleteItem(aItem);
-      delete this.mTaskCache[this.id][aItem.id];
       this.notifyOperationComplete(aListener,
                                     Components.results.NS_OK,
                                     Components.interfaces.calIOperationListener.DELETE,
@@ -246,31 +183,20 @@ calTodoTxt.prototype = {
                                     null,
                                     e.message);
     }
-  },
+  }
   
-  getItem: function cSC_getItem(aId, aListener) {
+  getItem(aId, aListener) {
     todotxtLogger.debug('calTodotxt.js:getItem()');
     // do we need to implement something here?
-  },
+  }
   
-  getItems: function cSC_getItems(aItemFilter, aCount, aRangeStart, aRangeEnd, aListener) {
+  getItems(aItemFilter, aCount, aRangeStart, aRangeEnd, aListener) {
     todotxtLogger.debug('calTodotxt.js:getItems()');
     // we have to initialize these, and the calendar ID property isn't available
     // when the constructor is called
-    if (!this.mTaskCache[this.id])
-      this.mTaskCache[this.id] = {};
-    
-    if (!this.mPendingApiRequestListeners[this.id])
-      this.mPendingApiRequestListeners[this.id] = [];
     
     try {
-      let items = todoClient.getItems(this, (this.mLastSync == null));
-
-      this.mLastSync = new Date();
-      this.mTaskCache[this.id] = {};
-
-      for (let item in items)
-        this.mTaskCache[this.id][item.id] = item;
+      let items = todoClient.getItems(this, true);
 
       aListener.onGetResult(this.superCalendar,
                             Components.results.NS_OK,
@@ -278,6 +204,7 @@ calTodoTxt.prototype = {
                             null,
                             items.length,
                             items);
+
       this.notifyOperationComplete(aListener, 
                                   Components.results.NS_OK,
                                   Components.interfaces.calIOperationListener.GET,
@@ -292,13 +219,5 @@ calTodoTxt.prototype = {
                                     null,
                                     e.message);
     }
-  },
-
-  startBatch: function cSC_startBatch(){
-    cal.LOG('calTodotxt.js:startBatch()');
-  },
-  
-  endBatch: function cSC_endBatch(){
-    cal.LOG('calTodotxt.js:endBatch()');
   }
-};
+}
